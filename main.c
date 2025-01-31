2283de18485d8481d08e34d4d3a5f4034a0150bf
wojciechaniol <162443006+wojciechaniol@users.noreply.github.com>
2025-01-31 12:39:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,49 @@
 #include "queue.h"
 
+char m1 = 'a', m2 = 'b', m3 = 'c', m4 = 'd', m5 = 'e', m6 = 'f', m7 = 'g', m8 = 'h'; 
+
 void* writerFunc2(void* arg)
 {
     TQueue* queue = (TQueue*)arg;
-    int j = 0, size = 1;
-    char* letter = "ABCDEFGHIJKLMNOPRSTUWYZabcdefghijklmnoprstuwyz";
-
-    while(1)
-    {
-        addMsg(queue, letter++);
-        addMsg(queue, letter++);
-        removeMsg(queue, &letter);
-        addMsg(queue, letter++);
-        j++;
-        if (j >= 15)
-        {
-            break;
-        }
-        setSize(queue, queue->maxSize+size);
-    }
+    int size = 2;
+    addMsg(queue, &m1);
+    addMsg(queue, &m2);
+    addMsg(queue, &m3);
+    removeMsg(queue, &m2);
+    addMsg(queue, &m4);
+    setSize(queue, size);
 
     return NULL;
 }
 
-void* readerFunc2(void* arg)
+void* T1(void* arg)
 {
     TQueue* queue = (TQueue*)arg;
     pthread_t self = pthread_self();
     subscribe(queue, self);
-	int j = 0;
 
-    while(1)
-    {
-		// getMsg(queue, self);
-        // getMsg(queue, self);
-		j++;
-        if (j == 20) 
-        {
-            unsubscribe(queue, self);
-            break;
-        }
-    }
-	unsubscribe(queue, self);
+    return NULL;
+}
+
+void* T2(void* arg)
+{
+    TQueue* queue = (TQueue*)arg;
+    pthread_t self = pthread_self();
+    void* msg;
+    subscribe(queue, self);
+    msg = getMsg(queue, self);
+    msg = getMsg(queue, self);
+
+    return NULL;
+}
+
+void* T3(void* arg)
+{
+    TQueue* queue = (TQueue*)arg;
+    pthread_t self = pthread_self();
+    void* msg;
+    subscribe(queue, self);
+    msg = getMsg(queue, self);
 
     return NULL;
 }
@@ -49,22 +51,20 @@ void* readerFunc2(void* arg)
 int main() 
 {
     // Initialize a queue with a maximum size
-    int maxSize = 2;
+    int maxSize = 3;
     TQueue* queue;
     queue = createQueue(maxSize);
 
     // Set up threads (4 subscribers and a writer)
-    pthread_t writer, subscriber1, subscriber2, subscriber3, subscriber4;
-    pthread_create(&subscriber1, NULL, readerFunc2, queue);
-    pthread_create(&subscriber2, NULL, readerFunc2, queue);
-    pthread_create(&subscriber3, NULL, readerFunc2, queue);
-    pthread_create(&subscriber4, NULL, readerFunc2, queue);
+    pthread_t writer, subscriber1, subscriber2, subscriber3;
+    pthread_create(&subscriber1, NULL, T1, queue);
+    pthread_create(&subscriber2, NULL, T2, queue);
+    pthread_create(&subscriber3, NULL, T3, queue);
     pthread_create(&writer, NULL, writerFunc2, queue);
 
     pthread_join(subscriber1, NULL);
     pthread_join(subscriber2, NULL);
     pthread_join(subscriber3, NULL);
-    pthread_join(subscriber4, NULL);
     pthread_join(writer, NULL);
 
     // Destroy the queue
